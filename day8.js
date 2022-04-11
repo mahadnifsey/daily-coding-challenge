c47203e42040a0c89318e8a1c299a808347d1268
mahadnifsey <mahadnifsey@gmail.com>
2022-04-11 13:13:05 +0100

    add day 8 coding file

---
+++ day8.js
@@ -0,0 +1,8 @@
+// String Repeat
+// Write a function called repeatStr which repeats the given string string exactly n times.
+function repeatStr (n, s) {
+    return s.repeat(n);
+  }
+
+  // Alternative Solution
+  repeatStr = (n, s) => s.repeat(n)
