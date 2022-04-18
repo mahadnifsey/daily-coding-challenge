df87c157f6bbf1062b953bdce97565a33a6bb1cd
mahadnifsey <mahadnifsey@gmail.com>
2022-04-18 21:52:15 +0100

    add day 15 coding file

---
+++ day13.js
@@ -0,0 +1,16 @@
+// Remove String Spaces
+// Simple, remove the spaces from the string, then return the resultant spring.
+function noSpace(x) {
+    let newStr = ''
+    for(let i = 0; i < x.length; i++) {
+        if(x[i] !== " "){
+            newStr += x[i]
+        }
+    }
+    return newStr
+}
+
+// Alternative Solutions
+function noSpace(x){
+    return x.replace(/\s/g, '');
+  }
