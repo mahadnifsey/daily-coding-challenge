b42e935db268f1a0b3d87406d616b6226adb7d74
mahadnifsey <mahadnifsey@gmail.com>
2022-05-10 20:17:29 +0100

    add day 43 coding file

---
+++ day-043.js
@@ -0,0 +1,11 @@
+// Find Maximum and Minimum Values of a List
+var min = function(list){
+    return Math.min(...list);
+}
+var max = function(list){
+    return Math.max(...list);
+}
+
+// Alternative Method
+const min = (list) => Math.min(...list);
+const max = (list) => Math.max(...list);
