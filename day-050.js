1381a9d64a482f2d5a781152ea12d9026423f25e
mahadnifsey <mahadnifsey@gmail.com>
2022-05-17 01:37:22 +0100

    resubmit day 50 coding file

--- day-050.js
+++ day-050.js
@@ -2,6 +2,12 @@
 // I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements.
 // I'll appreciate for your help.
 // P.S. Each array includes only integer numbers. Output is a number too.
+function arrayPlusArray(arr1, arr2) {
+    let arr = [...arr1, ...arr2];
+    return arr.reduce((a, b) => a + b);
+  };
+
+// Alternative Method
 function arrayPlusArray(arr1, arr2, arr3) {
     return arr3 = arr1.concat(arr2).reduce((x,y)=> x+y);
 };
