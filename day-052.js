eada4b6ca9eae2dbef86f2e5b6530fe882be0d71
mahadnifsey <mahadnifsey@gmail.com>
2022-05-19 23:59:19 +0100

    add day 54 coding file

---
+++ day-052.js
@@ -0,0 +1,8 @@
+// Function 3 - multiplying two numbers
+// Write here your multiply-function
+function multiply (x,y) {
+    return x*y;
+  };
+
+// Alternative Method
+const multiply = (x,y) => x*y;
---
+++ day-054.js
@@ -0,0 +1,6 @@
+// Convert a string to an array
+var stringToNumber = function(str){
+    // put your code here
+    return +str 
+   // ===  Number(str) || parseInt(str);
+}
