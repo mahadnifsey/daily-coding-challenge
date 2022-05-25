86c64d6c97ed60d392affcebfcdb63dc7b4f4462
mahadnifsey <mahadnifsey@gmail.com>
2022-05-25 04:02:51 +0100

    add day 60 coding file

---
+++ day-060.js
@@ -0,0 +1,12 @@
+// Area or Perimeter
+// You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+// If it is a square, return its area. If it is a rectangle, return its perimeter.
+const areaOrPerimeter = function(l , w) {
+    // If it's a square, the length and width are equal
+    if (l===w) return l*w
+    // If not return the perimeter of a rectangle.
+    return 2 * (l+w)
+  };
+
+// Alternative Method
+const areaOrPerimeter = (l , w) => l == w ? l*w : 2*(l+w)
