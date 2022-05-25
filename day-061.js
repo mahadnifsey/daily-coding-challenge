1e784b3d56b0c526838eeccce05c813ecd724d57
mahadnifsey <mahadnifsey@gmail.com>
2022-05-26 00:06:06 +0100

    add day 61 coding file update

--- day-061.js
+++ day-061.js
@@ -1,19 +1,22 @@
 // Rock Paper Scissors
 // Let's play! You have to return which player won! In case of a draw return Draw!.
 const rps = (p1, p2) => {
-    if (p1 === p2) {
-      return `Draw!`;
-    }
-    if (p1 === 'rock' && p2 === 'scissors') {
-      return `Player 1 won!`;
-    } else if (p1 === 'paper' && p2 === 'rock') {
-      return `Player 1 won!`;
-    } else if (p1 === 'scissors' && p2 === 'paper') {
-      return `Player 1 won!`;
+    // If there is a draw.
+    if(p1 === p2){
+      return 'Draw!';
+      // Player 1 win scenarios
+    } else if (p1 === 'rock' && p2 === 'scissors'){
+      return 'Player 1 won!';
+    } else if (p1 === 'scissors' && p2 === 'paper'){
+      return 'Player 1 won!';
+    } else if (p1 === 'paper' && p2 === 'rock'){
+      return 'Player 1 won!';
     } else {
-      return `Player 2 won!`;
+      // Player 2 win scenarios.
+      return 'Player 2 won!';
     }
   };
+  
 
   // Alternative Method
   const rps = (p1, p2) =>
