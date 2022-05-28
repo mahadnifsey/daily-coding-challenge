6749a53a057ca0b829d31fc4932c1450cb3fca6f
mahadnifsey <mahadnifsey@gmail.com>
2022-05-28 23:40:27 +0100

    add day 64 coding file

---
+++ day-064.js
@@ -0,0 +1,18 @@
+// Sum Mixed Array
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+function sumMix(x) {
+    // use the .isArray method to return 0
+    if (Array.isArray(x)) {
+        var sum = 0;
+        // Then use the .forEach to execute once for each element
+        x.forEach(function (element) {
+
+                sum += Number (element);
+        });
+    };
+    return sum;
+};
+// Alternative Method
+function sumMix(x){
+    return x.reduce((x, y) => Number(x) + Number(y), 0);
+}
