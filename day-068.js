8ac0aea804fc32681e29f9127dd93f3ad6f84aa9
mahadnifsey <mahadnifsey@gmail.com>
2022-05-31 23:30:56 +0100

    add day 68 coding file

---
+++ day-068.js
@@ -0,0 +1,15 @@
+// Find the first non-consecutive number
+// Your task is to find the first element of an array that is not consecutive.
+function firstNonConsecutive(arr) {
+    for (let i = 0; i < arr.length - 1; ++i) {
+      if (arr[i] + 1 !== arr[i + 1]) {
+        return arr[i + 1]
+      }
+    }
+    return null
+  };
+
+// Alternative Method
+function firstNonConsecutive (arr) {
+    return (r=arr.find((x,i)=>i>0&&x!=arr[i-1]+1))===undefined?null:r
+  }
