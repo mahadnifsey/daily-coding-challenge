e697b32110d58584c03265ffb0a2119c2daaac39
mahadnifsey <mahadnifsey@gmail.com>
2022-05-31 23:16:11 +0100

    add day 67 coding file.

---
+++ day-067.js
@@ -0,0 +1,30 @@
+// Sum without highest and lowest number
+// Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+// The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+function sumArray(array) {
+    if (array == null) {
+      return 0;
+    } else if (array.length < 2) {
+      return 0;
+    } else {
+      array = array.sort(function(a,b) {return a - b;});
+      var total = 0;
+      for (var i = 1; i < array.length - 1; i++) {
+        total += array[i];
+      }
+      return total;
+    }
+  };
+
+// Alternative Method
+function sumArray(array)  {
+    if(array && array.length > 1) {
+        const sortedArray = array.sort((a,b) => a - b).slice(1, -1)
+        return sortedArray.reduce((acc, cur) => acc + cur, 0)
+    }
+
+    return 0
+};
+
+// Alternative Method 2
+const sumArray = $ => !$ ? 0 : $.sort((a, b)=> a - b).slice(1,-1).reduce((acc, el)=> acc + el, 0)
