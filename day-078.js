ab866995f3907309f5a7f615cb52e71bac5ec832
mahadnifsey <mahadnifsey@gmail.com>
2022-06-10 23:17:49 +0100

    add day 78 coding file

---
+++ day-078.js
@@ -0,0 +1,7 @@
+// Grasshopper - Messi Goals
+// Use the following variables with a variable that will sum the total of all.
+var laLigaGoals = 43;
+var championsLeagueGoals = 10;
+var copaDelReyGoals = 5;
+
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
