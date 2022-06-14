f171488a9ad445c833467d35f27fd9264add6ebd
mahadnifsey <mahadnifsey@gmail.com>
2022-06-14 23:36:04 +0100

    update to day 80 coding file

--- day-080.js
+++ day-080.js
@@ -1,1 +1,56 @@
-// Daily Coding Challenge
+// Switch it Up!
+// When provided with a number between 0-9, return it in words.
+function switchItUp(number){
+    switch(number) {
+    case 0:
+      return "Zero";
+      break;
+    case 1:
+      return "One";
+      break;
+    case 2:
+      return "Two"
+      break;
+    case 3:
+      return "Three"
+      break;
+    case 4:
+      return "Four"
+      break;
+    case 5:
+      return "Five"
+      break;    
+    case 6:
+      return "Six"
+      break;
+    case 7:
+      return "Seven"
+      break;
+    case 8:
+      return "Eight"
+      break;      
+    case 9:
+      return "Nine"
+      break;
+    } 
+  };
+
+
+// Alternative Method
+switchItUp=n=>["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"][n]
+
+// Alternative Method 2
+function switchItUp(number) {
+    switch (number) {
+      case 0: return 'Zero';
+      case 1: return 'One';
+      case 2: return 'Two';
+      case 3: return 'Three';
+      case 4: return 'Four';
+      case 5: return 'Five';
+      case 6: return 'Six';
+      case 7: return 'Seven';
+      case 8: return 'Eight';
+      case 9: return 'Nine';
+    }
+  }
