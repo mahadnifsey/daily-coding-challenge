ec7f6159da7cbf2ab35a36e9aa36603b70e78580
mahadnifsey <mahadnifsey@gmail.com>
2022-06-14 23:26:21 +0100

    add day 82 coding file

---
+++ day-082.js
@@ -0,0 +1,9 @@
+// Expressions Matter
+// Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, ()
+// In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained
+function expressionMatter(a,b,c) {
+    return Math.max(a+b+c, (a+b)*c, a*(b+c), a*b*c);
+};
+
+// Alternative Method
+const expressionMatter = (a,b,c) => Math.max( a+b+c, (a+b)*c, a*(b+c), a*b*c );
