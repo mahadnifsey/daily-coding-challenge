8c4772c088d9ded5fa068f813f630b50e842cf5d
mahadnifsey <mahadnifsey@gmail.com>
2022-06-23 00:03:05 +0100

    update to day 92 coding file

--- day-092.js
+++ day-092.js
@@ -1,1 +1,30 @@
-// Answer Here
+// I love you, a little , a lot, passionately ... not at all
+// Your goal in this kata is to determine which phrase the girls would say for a flower of a 
+// given number of petals, where nb_petals > 0.
+function howMuchILoveYou(nbPetals) {
+    // your code
+  const arr = [
+     "I love you",
+     "a little",
+     "a lot",
+     "passionately",
+     "madly",
+     "not at all",
+   ];
+  
+  for (let i = 0; i <= nbPetals-1; i += 1) {
+    
+    if (i === nbPetals-1) {
+      return arr[i%6];
+    }
+  }
+};
+
+// Alternative Method
+const howMuchILoveYou=n=>['not at all','I love you','a little','a lot','passionately','madly'][n%6]
+
+// Alternative Method 2
+function howMuchILoveYou(nbPetals) {
+    let petals = ['I love you', 'a little', 'a lot', 'passionately', 'madly', 'not at all'];
+    return petals[(nbPetals-1) % 6];
+  };
