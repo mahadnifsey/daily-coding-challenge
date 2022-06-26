00077d89b2277e02bf0ca201f8ec828c7cce3d1c
mahadnifsey <mahadnifsey@gmail.com>
2022-06-26 22:53:39 +0100

    add day 97 coding file

---
+++ day-097.js
@@ -0,0 +1,9 @@
+// To be changed
+// Grasshopper - Debug sayHello
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+function sayHello(name) {
+    return `Hello, ${name}`
+}
+
+// Alternative Method
+const sayHello = name => `Hello, ${name}`;
