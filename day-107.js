051e5db99e1965f6aaa85914bda8faffa71d5d88
mahadnifsey <mahadnifsey@gmail.com>
2022-07-05 16:43:17 +0100

    add day 107 coding file

--- day-107.js
+++ day-107.js
@@ -1,1 +1,10 @@
-// Add Code Here
+// Regular Ball Super Ball
+var Ball = function(ballType) {
+    this.ballType = ballType || 'regular';
+  };
+
+// Alternative Method
+var Ball = function(ballType = "regular") {
+    this.ballType = ballType;
+  };
+  
