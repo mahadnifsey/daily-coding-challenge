6e4a1826ae4b73437eecbb179e4095bba4d7c18e
mahadnifsey <mahadnifsey@gmail.com>
2022-07-23 23:40:59 +0100

    update to day 98 coding file

--- day-098.js
+++ day-098.js
@@ -1,1 +1,15 @@
-// Answer Goes Here
+// Squash the bugs
+//
+function findLongest(str) {
+    let spl = str.split(" ");
+    let longest = 0;
+    for (let i = 0; i < spl.length; i++){
+      if (spl[i].length > longest) {
+        longest = spl[i].length;
+      }
+    }
+      return longest;
+  };
+
+// Alternative Method
+const findLongest = s => Math.max(...s.split(" ").map(x => x.length));
