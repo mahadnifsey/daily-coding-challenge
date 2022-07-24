5345ba2baea171f483fecd44fc9fb873535cee89
mahadnifsey <mahadnifsey@gmail.com>
2022-07-24 22:59:15 +0100

    update to day 99 coding file

--- day-099.js
+++ day-099.js
@@ -1,1 +1,19 @@
-// Code Goes here 
+// Well of Ideas - Easy Version
+// In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'.
+function well(x) {
+    switch (x.filter(i => i === 'good').length) {
+      case 0:
+        return 'Fail!'
+      case 1:
+      case 2:
+        return 'Publish!'
+      default:
+        return 'I smell a series!'
+    }
+  };
+
+// Alternative Method
+const  well = x => {
+    let count = x.filter(el => el == 'good').length
+    return  count > 2 ? 'I smell a series!' : count > 0 ? 'Publish!' : 'Fail!'; 
+};
