a6fd48213b8cbb048e77e62f78b4f8d4c77f5d34
mahadnifsey <mahadnifsey@gmail.com>
2022-07-25 23:24:02 +0100

    update day 100 coding file

--- day-100.js
+++ day-100.js
@@ -1,1 +1,12 @@
-// Add answer here
+// Filter out the geese
+// Write a function that takes a list of strings as an argument and returns a filtered list containing the same elements but with the 'geese' removed.
+function gooseFilter (birds) {
+    const geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    
+    return birds.filter((elm) => !geese.includes(elm))
+};
+
+// Alternative Method
+const gooseFilter = birds =>
+  (geese => birds.filter(val => !geese.includes(val)))
+  ([`African`, `Roman Tufted`, `Toulouse`, `Pilgrim`, `Steinbacher`]);
