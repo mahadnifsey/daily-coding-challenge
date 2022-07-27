8bc247ec6bde963b952e52321cf4ec533b067a7b
mahadnifsey <mahadnifsey@gmail.com>
2022-07-27 23:39:56 +0100

    update day 102 coding file

--- day-102.js
+++ day-102.js
@@ -1,2 +1,11 @@
-// Add code here
-// Updated
+// Drink about
+// Make a function that receive age, and return what they drink.
+function peopleWithAgeDrink(old){
+    return `drink ${old < 14 ? 'toddy' : old < 18 ? 'coke' : old < 21 ? 'beer' : 'whisky'}`;
+};
+
+// Alternative Method
+const peopleWithAgeDrink = (age) =>
+  age < 14 ? "drink toddy" :
+  age < 18 ? "drink coke" :
+  age < 21 ? "drink beer" : "drink whisky"
