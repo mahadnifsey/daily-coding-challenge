b52b77dbe2f7361cf9cc1bef539ed5babe070e7d
mahadnifsey <mahadnifsey@gmail.com>
2022-07-29 01:58:59 +0100

    update to day 103 coding file

--- day-103.js
+++ day-103.js
@@ -1,1 +1,11 @@
-// Code goes here
+// What's the real floor?
+// Write a function that given a floor in the american system returns the floor in the european system.
+const getRealFloor = n => {
+    if(n >= 13) return n - 2
+    if(n > 0) return n - 1
+    return n
+};
+// Alternative Method
+function getRealFloor(n) {
+    return n > 13 ? n - 2 : n > 0 ? n - 1 : n
+};
