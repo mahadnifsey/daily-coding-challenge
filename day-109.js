d28717afe384c4f676f3efd35c1f38627f42adb0
mahadnifsey <mahadnifsey@gmail.com>
2022-07-31 03:57:09 +0100

    update to day 109 coding file

--- day-109.js
+++ day-109.js
@@ -1,1 +1,15 @@
-// Add coding file here
+// Multiplication table for number
+// Your goal is to return multiplication table for number that is always an integer from 1 to 10.
+function multiTable(number) {
+    let table = "";
+
+    for(let i =1; i <=10; i++) {
+        let ans = i * number;
+        table += `${i} * ${number} = ${ans}\n`;
+    }
+
+    return table.trim("\n");
+};
+
+// Alternative Method
+multiTable=n=>[...Array(10)].map((_,i)=>(i++,i+' * '+n+' = '+(i*n))).join("\n");
