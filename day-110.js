dab9f72fe32ee2e1b55ecee9a350e3f477ac4d4e
mahadnifsey <mahadnifsey@gmail.com>
2022-08-01 05:20:26 +0100

    update to day 110 coding file

--- day-110.js
+++ day-110.js
@@ -1,1 +1,15 @@
-// Code goes here
+// Exclusive "or" (xor) Logical Operator
+function xor(a, b) {
+    if(a == true && b == false) {
+      return true;
+    } else if(a == false && b == true) {
+      return true;
+    } else {
+      return false;
+    }
+};
+
+// Alternative Method
+function xor(a, b) {
+    return a != b;
+};
