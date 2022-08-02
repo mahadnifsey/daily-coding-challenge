08a537af7a9f328978d2c7eb41e1ad2f0f1eb557
mahadnifsey <mahadnifsey@gmail.com>
2022-08-02 05:28:23 +0100

    update to day 111 coding file

--- day-111.js
+++ day-111.js
@@ -1,1 +1,23 @@
-// Add Code Here
+// Hello, Name or World!
+// Define a method hello that returns "Hello, Name!" to a given name,
+// or says Hello, World! if name is not given (or passed as an empty String).
+function hello(name) {
+    if (typeof name === "undefined" || name.length === 0) {
+      return `Hello, World!`;
+    } else {
+      return `Hello, ${name[0].toUpperCase()}${name.substring(1,).toLowerCase()}!`;
+    }
+};
+
+// Alternative Method
+function hello(name){
+    if (name){
+      return "Hello, " + name.substring(0,1).toUpperCase() + name.substring(1).toLowerCase() + '!';
+    } else {
+      return "Hello, World!";
+    }
+};
+
+// Alternative Method 2
+const hello = s =>
+  `Hello, ${s ? (s[0].toUpperCase() + s.slice(1).toLowerCase()) : 'World'}!`;
