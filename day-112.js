c9c77c25def31cc92e3b1302b4530cbc5dc3b895
mahadnifsey <mahadnifsey@gmail.com>
2022-08-03 05:39:11 +0100

    update to day 112 coding file

--- day-112.js
+++ day-112.js
@@ -1,1 +1,14 @@
-// Add code here.
+// Name Shuffler
+// Write a function that returns a string in which firstname is swapped with last name.
+function nameShuffler(str){
+    const fullName = str.split(" ");
+    return [fullName[1], fullName[0]].join(" ");
+};
+
+// Alternative Method
+function nameSuffle(str){
+    return str.split(' ').reverse().join(' ')
+};
+
+// Alternative Method 2
+const nameShuffler = str => str.split(' ').reverse().join(' ');
