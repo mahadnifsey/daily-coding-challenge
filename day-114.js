2194ebfbbb6e3bb70c7952fadfdc5bfe7c688df7
mahadnifsey <mahadnifsey@gmail.com>
2022-08-05 01:28:34 +0100

    update to day 114 coding file

--- day-114.js
+++ day-114.js
@@ -1,2 +1,14 @@
-// Answer goes here
-// Update
+// Filling an array (part 1)
+// Write a function that produces an array with the numbers 0 to N-1 in it.
+const arr = N => {
+    const arr = [];
+    
+    for(let i = 0; i < N; i+=1) {
+      arr.push(i);
+    }
+    
+    return arr;
+};
+
+// Alternative Method
+const arr = n => Array.from({length: n}, (_, i) => i);
