a4e9592d2e37abbc84af143bc2a1589acd53c61c
mahadnifsey <mahadnifsey@gmail.com>
2022-08-07 04:45:38 +0100

    update to day 116 coding file

--- day-116.js
+++ day-116.js
@@ -1,1 +1,14 @@
-// Add coding answer here 
+// Training JS #1: create your first JS function and print "Hello World!"
+
+function helloWorld() {
+    // creates variable called str to str that equals Hello World
+    var str = "Hello World!";
+    // prints the variable str to the console
+    console.log(str);
+};
+
+// Alternative Method
+helloWorld = function() {
+    var str = "Hello World!";
+    console.log( str );
+};
