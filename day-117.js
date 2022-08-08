34703ff2744c62d048ba5b54c3a29b2a1ebcecd6
mahadnifsey <mahadnifsey@gmail.com>
2022-08-08 14:52:16 +0100

    update to day 117 coding file

--- day-117.js
+++ day-117.js
@@ -1,1 +1,15 @@
-// Add coding answer here
+// Vowel remover
+// Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+function shortcut(str) {
+    return str.split('').filter(function(e) {
+      return ['a', 'e', 'i', 'o', 'u'].indexOf(e) == -1 
+    }).join('')
+};
+
+// Alternative Method
+function shortcut (string) {
+    return string.split(/[aeiou]/g).join('')
+};
+
+// Alternative Method 2
+const shortcut = string => string.replace( /[euioa]/g, '' );
