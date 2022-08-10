489a40f26a6f0db5e539fc64ededb2613187ff27
mahadnifsey <mahadnifsey@gmail.com>
2022-08-10 01:56:55 +0100

    update to day 118 coding file

--- day-118.js
+++ day-118.js
@@ -1,1 +1,19 @@
-// Add answer here
+// Find Multiples of a Number
+function findMultiples(integer, limit) {
+    const arr = [];
+    for (let i = 1; i <= limit; i += 1) {
+        if (integer * i <= limit) {
+          arr.push(integer * i);
+        }
+    }
+    return arr;
+};
+
+// Alternative Method
+function findMultiples(int,limit){
+    var array = [];
+    for (i=1; int*i <= limit; i++) {
+      array[i-1] = int*i;
+    }
+    return array;
+};
