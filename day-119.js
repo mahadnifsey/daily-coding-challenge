0f4f7a0bf8094c986e35fbbf65e0cee11de7d8c6
mahadnifsey <mahadnifsey@gmail.com>
2022-08-11 03:54:20 +0100

    update to day 119 coding file

--- day-119.js
+++ day-119.js
@@ -1,1 +1,20 @@
-// Add answer here
+// Lario and Muigi Pipe Problem
+function pipeFix(numbers){
+	let arr = [];
+	for (let i = numbers[0]; i <= numbers[numbers.length - 1]; i++) {
+		arr.push(i);
+	}
+	return arr;
+};
+
+// Alternative Method
+function pipeFix(numbers){
+    var first = numbers[0];
+    var last = numbers[numbers.length-1];
+    
+    var arr = [];
+    for(var i = first; i <= last; i++) {
+      arr.push(i);
+    }
+    return arr;
+};
