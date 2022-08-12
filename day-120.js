e1b4f1d74308b43cbb0802203ec5a9170f46a986
mahadnifsey <mahadnifsey@gmail.com>
2022-08-12 03:58:52 +0100

    update to day 120 coding file

--- day-120.js
+++ day-120.js
@@ -1,1 +1,11 @@
-// Add answer here
+// Holiday VIII - Duty Free
+function dutyFree(normPrice, discount, hol){
+    var discountAmount = normPrice * (discount / 100);
+    var bottles = hol / discountAmount;
+    return Math.floor(bottles);
+};
+
+// Alternative Method
+  function dutyFree(normPrice, discount, hol){
+    return Math.floor(hol/((normPrice*discount)/100));
+};
