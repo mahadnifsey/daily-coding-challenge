a3871f0c3f0f42558c4bbcc38adedccc537db8b8
mahadnifsey <mahadnifsey@gmail.com>
2022-08-13 04:03:51 +0100

    update to day 121 coding file

--- day-121.js
+++ day-121.js
@@ -1,1 +1,18 @@
-// Add answer here
+// Convert to Binary
+function toBinary(n){
+    var s = n.toString(2);
+    var c = Number(s);
+    return c;
+};
+
+// Alternative Method
+function toBinary(n){
+    let arr = []
+     for (let i = n; i >= 1; i = Math.floor(i / 2)) {
+       arr.push(i % 2 === 0 ? 0 : 1)
+       }
+   return Number(arr.reverse().join(''))
+};
+
+// Alternative Method 2
+let toBinary = n => +n.toString(2)
