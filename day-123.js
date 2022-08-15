e29a20563be56b8a60c2100881558eb94ebfdac7
mahadnifsey <mahadnifsey@gmail.com>
2022-08-15 03:52:29 +0100

    update to day 123 coding file

--- day-123.js
+++ day-123.js
@@ -1,1 +1,12 @@
-// Add answer here
+// Hex to Decimal
+// Complete the function which converts hex number (given as a string) to a decimal number.
+function hexToDec(hexString){
+    return parseInt(hexString,16);
+};
+
+// Alternative Method
+function hexToDec(hexString){
+    var num = 0;
+    num = parseInt(hexString, 16)
+    return num;
+};
