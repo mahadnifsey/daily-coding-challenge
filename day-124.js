667860e2d23f447637ca0ef3853cb53d9fed3813
mahadnifsey <mahadnifsey@gmail.com>
2022-08-16 05:40:55 +0100

    update to 124 coding file

--- day-124.js
+++ day-124.js
@@ -1,1 +1,17 @@
-// Add answer here
+// Gravity Flip
+// Given the initial configuration of the cubes in the box, 
+// find out how many cubes are in each of the n columns after Bob switches the gravity.
+const flip = (d, a)=>{
+    if (d === 'R') {
+        return a.sort((a, b) => a -b);
+    }
+
+    return a.sort((a, b) => b -a);
+};
+
+// Alternative Method
+const flip = (d, a) =>
+  a.sort((a, b) => d === `R` ? a - b : b - a);
+
+// Alternative method 2
+const flip = (d, a) => d === 'L' ? a.sort((x, y) => y-x) : a.sort((x,y)=> x-y)
