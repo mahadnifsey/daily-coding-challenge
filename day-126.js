0afa96e5f8dcc619b829412fff7e90e5b955202b
mahadnifsey <mahadnifsey@gmail.com>
2022-08-18 03:03:21 +0100

    update to day 126 coding file

--- day-126.js
+++ day-126.js
@@ -1,1 +1,21 @@
-// Add answer here
+// 101 Dalmatians -squash the bugs, not the dogs!
+// Your friend has been out shopping for puppies (what a time to be alive!)... He arrives back with multiple dogs, and you simply do not know how to respond!
+// By repairing the function provided, you will find out exactly how you should respond, depending on the number of dogs he has.
+// The number of dogs will always be a number and there will always be at least 1 dog.
+function howManyDalmatians(number){
+    if (number <= 10) {
+      return "Hardly any"
+    } else if (number <= 50) {
+      return "More than a handful!"
+    } else if (number === 101) {
+      return "101 DALMATIANS!!!"
+    } else {
+      return "Woah that's a lot of dogs!" 
+    }
+};
+
+// Alternative Method
+function howManyDalmatians(number) {
+    var dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"];
+    return number <= 10 ? dogs[0] : number <= 50 ? dogs[1] : number == 101 ? dogs[3] : dogs[2]
+};
