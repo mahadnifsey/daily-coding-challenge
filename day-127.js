830728074f6933f60dc9c1c42e8401de7d597e4b
mahadnifsey <mahadnifsey@gmail.com>
2022-08-19 02:33:28 +0100

    update to day 127 coding file

--- day-127.js
+++ day-127.js
@@ -1,1 +1,18 @@
-// Add answer here
+// Enumerable Magic #25 - Take the First N Elements
+function take(arr, n) {
+    let counter = 0;
+    let res = [];
+    for (let i of arr) {
+      counter += 1;
+      if (counter <= n && counter <= arr.length) {
+        res.push(i);
+      }
+    }
+    return res;
+};
+
+// Alternative Method
+
+function take(arr, n) {
+    return arr.slice(0, n);
+};
