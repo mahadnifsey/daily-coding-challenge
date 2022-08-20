b2efb1b96c6460b537c494841861c3ed72894eb0
mahadnifsey <mahadnifsey@gmail.com>
2022-08-20 04:35:04 +0100

    update to day 128 coding file

--- day-128.js
+++ day-128.js
@@ -1,1 +1,23 @@
-// Add answer here
+// Palindrome Strings
+// A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward.
+// This includes capital letters, punctuation, and word dividers.
+// Implement a function that checks if something is a palindrome. If the input is a number, convert it to string first. 
+function isPalindrome(line) {
+    line = line.toString();
+    
+    for (var left = 0, right = line.length - 1; left < right;) {
+      if (line.charAt(left++) !== line.charAt(right--)) {
+        return false;
+      }
+    }  
+    return true;
+};
+
+// Alternative Method
+function isPalindrome(line) {
+    return (String(line) == String(line).split('').reverse().join('') )
+};
+
+// Alternative Method 2
+isPalindrome = line => String(line) == String(line).split('').reverse().join('')
+
