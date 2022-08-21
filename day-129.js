34080d474b9ece201ca026b2e3df853c3bd3b818
mahadnifsey <mahadnifsey@gmail.com>
2022-08-21 06:36:34 +0100

    update to day 129 coding file

--- day-129.js
+++ day-129.js
@@ -1,1 +1,11 @@
-// Add answer here.
+// Grasshopper - Function syntax debugging
+// A student was working on a function and made some syntax mistakes
+// while coding. Help them find their mistakes and fix them.
+function main (verb, noun) {
+    return verb + noun
+};
+
+// Alternative Method
+function main (verb, noun){
+    return verb + noun
+};
