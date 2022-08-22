8e817e2c696ebdde76c22588bb856e97005d9513
mahadnifsey <mahadnifsey@gmail.com>
2022-08-22 04:44:55 +0100

    update to day 130 coding file

--- day-130.js
+++ day-130.js
@@ -1,1 +1,16 @@
-// Add answer here.
+// Remove duplicates from list
+// Define a function that removes duplicates from an array of numbers and returns it as a result.
+// The order of the sequence has to stay the same.
+function distinct(arr) {
+    let res = []; 
+    
+    for(let i = 0; i < arr.length; i++){
+      if(!res.includes(arr[i])){
+        res.push(arr[i]);
+      }
+  }
+  return res;
+};
+
+// Alternative Method
+const distinct = a => a.filter((item, index) => a.indexOf(item) === index);
