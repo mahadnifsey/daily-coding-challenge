a958e550ca5665ce08353b5925bd8c7e1125c4da
mahadnifsey <mahadnifsey@gmail.com>
2022-08-25 17:47:31 +0100

    update to day 133 coding file

--- day-133.js
+++ day-133.js
@@ -1,1 +1,18 @@
-// Add answer here.
+// Find the Remainder
+function remainder(a, b){
+    // Divide the larger argument by the smaller argument and return the remainder
+    if(b>a)
+      return b%a;
+    else
+      return a%b;
+};
+
+// Alternative Method
+function remainder(a, b){
+    return a > b ? a % b : b % a;
+};
+
+// Alternative Method 2
+function remainder(a, b){  
+    return Math.max(a,b) % (Math.min(a,b) || NaN);
+};
