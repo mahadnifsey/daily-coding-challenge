031125eae8a232a2b710bbf468c993e7086cb327
mahadnifsey <mahadnifsey@gmail.com>
2022-08-26 15:28:15 +0100

    update to day 134 coding file

--- day-134.js
+++ day-134.js
@@ -1,1 +1,13 @@
-// Add answer here.
+// Bin to Decimal
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+function binToDec(bin){
+    return parseInt(bin,2);
+};
+
+// Alternative Method
+function binToDec(bin){
+    return bin.split('')
+      .reverse()
+        .map((x,i)=>x==1?Math.pow(2,i):0)
+          .reduce((a,b)=>a+b,0);
+};
