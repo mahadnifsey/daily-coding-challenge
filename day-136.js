79e8401632b958302400c957843604b9cc299a06
mahadnifsey <mahadnifsey@gmail.com>
2022-08-28 04:52:56 +0100

    add day 159 coding file

--- day-136.js
+++ day-136.js
@@ -1,1 +1,15 @@
-// Add answer here.
+// Alan Partridge II - Apple Turnover
+// Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, 
+// else, return Help yourself to a honeycomb Yorkie for the glovebox.
+function apple(x){
+    if(Number(x)**2 > 1000){
+      return 'It\'s hotter than the sun!!';
+    }else{
+      return 'Help yourself to a honeycomb Yorkie for the glovebox.';
+      }
+};
+
+// Alternative Method
+function apple(x){
+    return Math.pow(x, 2) > 1000 ? 'It\'s hotter than the sun!!' : 'Help yourself to a honeycomb Yorkie for the glovebox.' ;
+};
