ec6f510ef81f785ede8e9d401f97a59dd52b92b0
mahadnifsey <mahadnifsey@gmail.com>
2022-08-29 19:03:31 +0100

    update to day 137 coding file

--- day-137.js
+++ day-137.js
@@ -1,1 +1,6 @@
-// Add answer here.
+// 5 without number!!!
+// Write a function that always returns 5
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+function unusualFive() {
+    return ['one', 'two', 'three', 'four', 'five'].length;
+};
