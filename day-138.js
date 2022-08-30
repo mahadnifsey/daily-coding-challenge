2e9013123364bbc5530c7ca4564bb8403b725fe9
mahadnifsey <mahadnifsey@gmail.com>
2022-08-31 00:09:58 +0100

    update to day 138 coding file

--- day-138.js
+++ day-138.js
@@ -1,1 +1,12 @@
-// Add answer here.
+// Surface Area and Volume of a Box
+// Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+const getSize = (width, height, depth) => [
+    ((width * height) + (width * depth) + (depth * height)) * 2,
+    width * height * depth
+];
+
+// Alternative Method
+const getSize = (w, h, d) => [
+    (w * h + w * d + h * d) * 2,
+    w * h * d
+];
