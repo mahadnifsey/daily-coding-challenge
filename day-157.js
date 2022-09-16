0242d2e51ae939c63b702d1925e6e00cd6a7fa1a
mahadnifsey <mahadnifsey@gmail.com>
2022-09-16 19:37:12 +0100

    update to day 157 coding file

--- day-157.js
+++ day-157.js
@@ -1,1 +1,19 @@
-// Add answer here.
+// USD => CNY
+// Create a function that converts US dollars (USD) to Chinese Yuan (CNY) . 
+// The input is the amount of USD as an integer, and the output should be a string that states 
+// the amount of Yuan followed by 'Chinese Yuan'
+function usdcny(usd) {
+    return `${(usd * 6.75).toFixed(2)} Chinese Yuan`
+};
+
+// Alternative Method
+function usdcny(usd) {
+    let con = usd * 6.75
+    return `${con.toFixed(2)} Chinese Yuan`
+};
+
+// Alternative Method 2
+const usdcny = usd => `${(usd*6.75).toFixed(2)} Chinese Yuan`;
+
+// Alternative Method 3
+usdcny = $ => `${($ * 6.75).toFixed(2)} Chinese Yuan`;
