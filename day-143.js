4c5fd66705eb27c577f1544142b5d002617fda10
mahadnifsey <mahadnifsey@gmail.com>
2022-09-17 22:00:18 +0100

    update to day 143 coding file

--- day-143.js
+++ day-143.js
@@ -1,1 +1,7 @@
-// Add answer here.
+// Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence
+function replace(s){
+    return s.replace(/[aeoiu]/ig, '!')  
+};
+
+// Alternative Method
+const replace = s => s.replace(/[aeiou]/gi, '!');
