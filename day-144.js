2531faaa69f0933f19d6cd30507c24619530d92c
mahadnifsey <mahadnifsey@gmail.com>
2022-09-18 06:30:00 +0100

    update to day 144 coding file

--- day-144.js
+++ day-144.js
@@ -1,1 +1,12 @@
-// Add answer here.
+// No zeros for heros
+function noBoringZeros(n) {
+    while (n % 10 === 0 && n !== 0) {
+      n = n / 10
+    }
+    return n
+};
+
+// Alternative Method
+function noBoringZeros(n) {
+    return Number(n.toString(). replace(/0+$/, ''));
+};
