439a6a620c386d1d85b5a3a23631431584b2cb88
mahadnifsey <mahadnifsey@gmail.com>
2022-09-19 09:31:10 +0100

    update to day 145 coding file

--- day-145.js
+++ day-145.js
@@ -1,1 +1,14 @@
-// Add answer here.
+// Swap Values
+// I would like to be able to pass an array with two elements to my swapValues function to swap the values. However it appears that the values aren't changing.
+// Can you figure out what's wrong here?
+function swapValues() {
+    var args = arguments[0];
+    var temp = args[0];
+    args[0] = args[1];
+    args[1] = temp;
+};
+
+// Alternative Method
+function swapValues(arr) {
+    return arr.reverse();
+};
