e2cbdc633829785b9f43f81c6b77e3c8755c7094
mahadnifsey <mahadnifsey@gmail.com>
2022-09-20 01:32:03 +0100

    update to day 146 coding file

--- day-146.js
+++ day-146.js
@@ -1,1 +1,12 @@
-// Add answer here.
+// Sleigh Authentication
+function Sleigh() {}
+Sleigh.prototype.authenticate = function(name, password) {
+    return name == 'Santa Claus' && password == "Ho Ho Ho!";
+};
+
+// Alternative Method
+class Sleigh {
+    authenticate(name, password) {
+        return name == "Santa Claus" && password == "Ho Ho Ho!";
+    }
+};
