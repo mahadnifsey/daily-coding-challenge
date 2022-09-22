ee798889fd73321cebc40137e091c0b256e2ac8b
mahadnifsey <mahadnifsey@gmail.com>
2022-09-22 22:34:06 +0100

    update to day 148 coding file

--- day-148.js
+++ day-148.js
@@ -1,1 +1,36 @@
-// Add answer here.
+// Training JS #2: Basic data types--Number
+var v1=50;v2=100,v3=150,v4=200,v5=2,v6=250
+function equal1(){
+    var a=v1
+    var b=v1
+    return a+b;
+};
+function equal2(){
+    var a=v3
+    var b=v1
+    return a-b;
+};
+function equal3(){
+    var a=v1
+    var b=v5
+    return a*b;
+};
+function equal4(){
+    var a=v4
+    var b=v5
+    return a/b;
+};
+function equal5(){
+    var a=v6
+    var b=v3
+    return a%b;
+};
+
+// Alternative Method
+var v1=50;v2=100,v3=150,v4=200,v5=2,v6=250
+
+const equal1 = () => v1 + v1
+const equal2 = () => v3 - v1
+const equal3 = () => v1 * v5
+const equal4 = () => v4 / v5
+const equal5 = () => v2 % v4
