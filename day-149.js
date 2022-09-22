84930de5a17d243d0fc0aa7c311c0a7fc59cd0b8
mahadnifsey <mahadnifsey@gmail.com>
2022-09-23 00:35:11 +0100

    update to day-149.js

--- day-149.js
+++ day-149.js
@@ -1,1 +1,7 @@
-// Add answer here.
+// Printing Array elements with Comma delimiters
+function printArray(array){
+    return array.join();
+};
+
+// Alternative Method
+const printArray=array=>array.join();
