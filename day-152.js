fa54d016295820632860bcbe3b64b93f81b83db3
mahadnifsey <mahadnifsey@gmail.com>
2022-09-26 19:23:52 +0100

    update to day 152 coding file

--- day-152.js
+++ day-152.js
@@ -1,1 +1,5 @@
-// Add answer here.
+// Determine offspring sex based on genes XX and XY chromosomes
+Function chromosomeCheck(sperm){
+    if(sperm === 'XY') return `Congratulations! You're going to have a son.` 
+    return `Congratulations! You're going to have a daughter.`
+}
