6ca34e1e0fd2ce687ea7b1053c4cecc692b133e7
mahadnifsey <mahadnifsey@gmail.com>
2022-09-27 02:08:33 +0100

    update to day 153 coding file

--- day-153.js
+++ day-153.js
@@ -1,1 +1,25 @@
-// Add answer here.
+// Pre-FizzBuzz Workout #1
+function preFizz(n) {
+    let output = new Array();
+    let num = 1;
+    while(output.length  < n){
+      output.push(num);
+      num += 1;
+    }
+    return output;
+};
+
+// Alternative Method 2
+function preFizz(n) {
+    var arr = [];
+    for(var i = 1; i < n + 1; i++){
+      arr.push(i);
+    }
+    return arr;
+};
+
+// Alternative Method 3
+preFizz = n => new Array( n ).fill().map( (val, index) => index + 1 );
+
+// Alternative Method 4
+const preFizz = n => Array.from({length: n}, (_, i) => i + 1)
