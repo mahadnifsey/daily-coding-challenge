0f7b8cfe9a9ef24b8f00b8572a68f00af4505b6f
mahadnifsey <mahadnifsey@gmail.com>
2022-09-29 22:46:45 +0100

    update to day 154 coding file

--- day-154.js
+++ day-154.js
@@ -1,1 +1,6 @@
-// Add answer here.
+// Basic Training: Add item to an Array
+// Add the value "codewars" to the websites array.
+websites.push('codewars');
+
+// Alternative Method
+websites.push('codewars')
