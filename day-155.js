337e69aab6d18e0a9488a930add82f2c10a0bdd4
mahadnifsey <mahadnifsey@gmail.com>
2022-09-30 02:50:08 +0100

    update to day 155 coding file

--- day-155.js
+++ day-155.js
@@ -1,1 +1,13 @@
-// Add answer here.
+// Reversing Words in a String
+// As the input may have trailing spaces, you will also need to ignore unneccesary whitespace.
+function reverse(string){
+    return string.split(' ').reverse().join(' ');
+};
+
+// Alternative Method
+function reverse(string){
+    return string.trim().split(/ +/).reverse().join(' ');
+};
+
+// Alternative Method 2
+reverse = string => string.split(' ').reverse().join(' ')
