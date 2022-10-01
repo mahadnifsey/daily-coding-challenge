dc2b1ee34452532c4f361575cc8d9573aa1ce374
mahadnifsey <mahadnifsey@gmail.com>
2022-10-01 13:27:37 +0100

    update to day 156 coding file

--- day-156.js
+++ day-156.js
@@ -1,1 +1,15 @@
-// Add answer here.
+// Remove First and Last Character Part Two
+// Write a function which returns a new string containing 
+// the same character sequences except the first and the last ones but this time separated by spaces.
+function array(arr){
+    return arr.split(",").slice(1,-1).join(" ") || null;
+};
+
+// Alternative Method
+const array = str => {
+    var arr = str.split(",");
+    return arr.length > 2 ? arr.slice(1,-1).join(" ") : null;
+};
+
+// Alternative Method 2
+array = arr => arr.split(',').slice(1,-1).join(' ') || null
