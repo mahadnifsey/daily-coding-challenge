56bc438688dd73d083268c680117af9b24ec63f0
mahadnifsey <mahadnifsey@gmail.com>
2022-10-03 18:26:44 +0100

    update to day 159 coding file

--- day-159.js
+++ day-159.js
@@ -1,1 +1,11 @@
-// Add answer here.
+// Area of a Square
+// Complete the function that calculates the area of the red square, 
+// when the length of the circular arc A is given as the input. 
+// Return the result rounded to two decimals.
+function squareArea(A){
+    return +Math.pow(2 * A / Math.PI, 2).toFixed(2)
+};
+
+// Alternative Method
+squareArea = A => +Math.pow((2 * A / 3.1416), 2).toFixed(2);
+
