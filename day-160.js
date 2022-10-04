672af49144465ecbd93fd485e5beccab540677d9
mahadnifsey <mahadnifsey@gmail.com>
2022-10-04 16:28:18 +0100

    update to day 160 coding file

--- day-160.js
+++ day-160.js
@@ -1,1 +1,10 @@
-// Add answer here.
+// Kata Example Twist
+// This is an easy twist to the example kata 
+// (provided by Codewars when learning how to create your own kata). 
+const websites=Array(1000).fill({
+    node_modules: Array(1000000).fill('jquery'),
+    toString: () => 'codewars'
+});
+
+// Alternative Method
+const websites = Array(1000).fill('codewars')
