7297236a7a0f043985ab919655a5c9edb5480d8b
mahadnifsey <mahadnifsey@gmail.com>
2022-10-06 22:31:19 +0100

    update to day 162 coding file

--- day-162.js
+++ day-162.js
@@ -1,1 +1,10 @@
-// Add answer here.
+// Find the position
+function position(letter){
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    return 'Position of alphabet: ' + (alphabet.indexOf(letter) + 1);
+};
+
+// Alternative Method
+function position(letter) {
+    return `Position of alphabet: ${letter.charCodeAt(0) -96}`
+};
