9509649638f40d72821860fedb1f0d81071e3358
mahadnifsey <mahadnifsey@gmail.com>
2022-10-08 08:05:20 +0100

    update to day 164 coding file

--- day-164.js
+++ day-164.js
@@ -1,1 +1,11 @@
-// Add Code Here
+// Holiday VI - Shark Pontoon
+const shark = (pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin) => {
+    let youTime = pontoonDistance / youSpeed
+    let sharkTime = sharkDistance / (dolphin ? sharkSpeed / 2 : sharkSpeed)
+    
+    return sharkTime < youTime ? 'Shark Bait!' : 'Alive!'
+};
+
+function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin){
+    return sharkDistance/(dolphin?sharkSpeed/2:sharkSpeed) > pontoonDistance/youSpeed ? "Alive!":"Shark Bait!";
+};
