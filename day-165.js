d118d17468c73ef8688dcfd19114d1131c0b04a8
mahadnifsey <mahadnifsey@gmail.com>
2022-10-09 02:54:02 +0100

    update to day 165 coding file

--- day-165.js
+++ day-165.js
@@ -1,1 +1,10 @@
-// Add Code Here
+// Enumerable Magic - Does My List Include This?
+// Create a method that accepts a list and an item, 
+// and returns true if the item belongs to the list, otherwise false.
+function include(arr, item){
+    return arr.includes(item);
+};
+
+
+// Alternative Method
+const include = (arr, item) => arr.includes(item);
