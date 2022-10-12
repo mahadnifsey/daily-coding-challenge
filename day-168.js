93447e5dd0c1e61a81346d940575704bd2b99957
mahadnifsey <mahadnifsey@gmail.com>
2022-10-12 18:21:50 +0100

    update to day 168 coding file

--- day-168.js
+++ day-168.js
@@ -1,1 +1,17 @@
-// Add Code Here
+// How many stairs will Suzuki climb in 20 years?
+function stairsIn20(s){
+    for (var i = 0, sum = 0; i<s.length ; i++) {
+      for (var j = 0; j<s[i].length; j++) {
+        sum += s[i][j];
+      }
+    }
+    return sum*20;
+};
+
+// Alternative Method
+stairsIn20 = s => 20 * s.reduce((prev1,curr1) => prev1 + curr1.reduce((prev2,curr2) => prev2 + curr2, 0), 0)
+
+// Alternative Method 2
+function stairsIn20(s){
+    return s.reduce((a,b) => a.concat(b)).reduce((a,b) => a + b) * 20;
+};
