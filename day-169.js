6a60b02e6ff0eb75948e4d00f474d839bef39ef0
mahadnifsey <mahadnifsey@gmail.com>
2022-10-13 01:15:39 +0100

    update to day 169 coding file

--- day-169.js
+++ day-169.js
@@ -1,1 +1,20 @@
-// Add Code Here
+// Grasshopper - Array Mean
+// Find the mean (average) of a list of numbers in an array.
+var findAverage = function (nums) {
+    // Code here
+    var sum = 0;
+    for (num in nums) {
+      sum += nums[num];
+    }
+    return sum / (nums.length);
+};
+
+// Alternative Method
+const findAverage = function (nums) {
+    return nums.reduce(function(a,b){
+      return a+b;
+    }) / nums.length;
+};
+
+// Alternative Method 2
+const findAverage = nums => nums.reduce((a, b) => a + b) / nums.length;
