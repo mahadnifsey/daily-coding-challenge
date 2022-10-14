9490254ac8fa44b26aa720a5f4be595bf922812e
mahadnifsey <mahadnifsey@gmail.com>
2022-10-14 15:03:58 +0100

    update to day 170 coding file

--- day-170.js
+++ day-170.js
@@ -1,1 +1,15 @@
-// Add Code Here
+// Color Ghost
+// Create a class Ghost
+// Ghost objects are instantiated without any arguments.
+// Ghost objects are given a random color attribute of "white"
+//  or "yellow" or "purple" or "red" when instantiated
+var Ghost = function() {
+    this.color = ['white', 'yellow', 'red', 'purple'][Math.floor(Math.random() *4)]
+};
+
+// Alternative Method
+var Ghost = function() {
+    var colors = ["white", "yellow", "purple", "red"];
+    var colorIndex = Math.floor(Math.random() * (colors.length));
+    this.color = colors[colorIndex];
+};
