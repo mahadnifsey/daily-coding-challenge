00efa8327560713646a17cb45e28f117a42c22cf
mahadnifsey <mahadnifsey@gmail.com>
2022-10-18 23:02:29 +0100

    update to day 174 coding file

--- day-174.js
+++ day-174.js
@@ -1,1 +1,19 @@
-// Add Code Here
+// Training JS #4: Basic data types--Array
+function getLength(arr){
+    return arr.length
+}
+function getFirst(arr){
+    return arr[0]
+}
+function getLast(arr){
+    return arr[arr.length -1]
+}
+function pushElement(arr){
+    var el=1;
+    arr.push(el)
+    return arr
+}
+function popElement(arr){
+    arr.pop()
+    return arr
+};
