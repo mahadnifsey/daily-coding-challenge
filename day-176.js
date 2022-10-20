f9340d96297327ab8fde80e84007aeca43692b8b
mahadnifsey <mahadnifsey@gmail.com>
2022-10-20 18:55:32 +0100

    update to day 176 coding file

--- day-176.js
+++ day-176.js
@@ -1,1 +1,27 @@
-// Add Code Here
+// Basic subclasses - Adam and Eve
+class God{
+    /**
+     * @returns Human[]
+     */
+      static create(){
+        return [new Man, new Woman];
+      }
+};
+    class Human{}
+    class Man extends Human{}
+    class Woman extends Human{}
+
+// Alternative Method
+class Human{}
+class Man{}
+class Woman{}
+
+Object.setPrototypeOf(Man.prototype, Human.prototype);
+Object.setPrototypeOf(Woman.prototype, Human.prototype);
+class God {
+  static create() {
+    // write code here
+
+    return [new Man(), new Woman()];
+  }
+};
