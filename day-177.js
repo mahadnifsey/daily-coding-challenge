847c59cdbc62b3919ffbbaf31f291250ec113d3e
mahadnifsey <mahadnifsey@gmail.com>
2022-10-21 02:37:52 +0100

    update to day 177 coding file

--- day-177.js
+++ day-177.js
@@ -1,1 +1,9 @@
-// Add Code Here
+// Mr.Freeze
+// There is an object/class already created called MrFreeze. 
+// Mark this object as frozen so that no other changes can be made to it.
+Object.freeze(MrFreeze);
+
+
+// Alternative Method
+var MyFreeze = {};
+(Object.freeze || object)(MrFreeze);
