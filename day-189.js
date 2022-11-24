3ff3d25e65827047c26ecdcb1eb05ae6dd2c3465
mahadnifsey <mahadnifsey@gmail.com>
2022-11-24 09:21:49 +0000

    update to day 189 coding file

--- day-189.js
+++ day-189.js
@@ -1,1 +1,16 @@
-// Enter Code Here
+// Sum of Multiples
+// Find the sum of all multiples of n below m.
+function sumMul(n,m){
+    if (n >= m) return "INVALID";
+
+  let sum = 0;
+    for (let i = n; i < m; i+=n) {
+      sum += i;
+    }
+    return sum;
+};
+
+// Alternative Method
+function sumMul(n,m){
+    return m<=n ? "INVALID" : Math.floor(m/n)*(Math.floor(m/n)+1)*(n/2);
+};
