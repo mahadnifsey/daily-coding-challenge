b252f04261486ee052277421d4304a6f4d00775f
mahadnifsey <mahadnifsey@gmail.com>
2022-11-26 15:15:36 +0000

    Update to day 190 coding file

--- day-190.js
+++ day-190.js
@@ -1,1 +1,8 @@
-// Enter Code Here
+// A Strange Trip to the Market
+//FIND THE LOCH NESS MONSTER. SAVE YOUR TREE FIDDY
+function isLockNessMonster(s) {
+    return /tree fiddy|3.50|three fifty/.test(s)
+};
+
+// Alternative Method
+const isLockNessMonster = (s) => /tree fiddy|three fifty|3.50/.test(s);
