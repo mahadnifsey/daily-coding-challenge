7e0aa049e660ddee1808265ece1bca0fb03c6168
mahadnifsey <mahadnifsey@gmail.com>
2022-11-27 23:02:06 +0000

    Update to day 191 coding file

--- day-191.js
+++ day-191.js
@@ -1,1 +1,12 @@
-// Enter Code Here
+// Grasshopper - Combine strings
+function combineNames(first, last) {
+    return first + " " + last;
+};
+
+// Alternative Method - Arrow Function
+combineNames = (a, b) => {
+    return a + " " + b;
+};
+
+// Alternative Method 2
+combineNames = (first,last) => `${first} ${last}`
