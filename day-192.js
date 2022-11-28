1071f7af02d2488b97bde5ef545152cd845f4194
mahadnifsey <mahadnifsey@gmail.com>
2022-11-28 15:55:27 +0000

    Update to day 192 coding file

--- day-192.js
+++ day-192.js
@@ -1,1 +1,13 @@
-// Enter Code Here
+// Is it a number?
+function isDigit(s) {
+    return s.trim() === '' ? false : !isNaN(s.trim());
+};
+
+// Alternative Method
+// parseFloat - Ignores whitespace and only focuses on the numbers eg; 4534nfsdf - it will only focus on 4534.
+function isDigit(s) {
+    return s==parseFloat(s);
+};
+
+// Alternative Method 2
+const isDigit = s => /^-?\d+(\.\d+)?$/.test(s);
