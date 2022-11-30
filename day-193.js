4ae3476f3aa00bb10adf3532fdf479b4538bb6cd
mahadnifsey <mahadnifsey@gmail.com>
2022-11-30 23:17:24 +0000

    Update to day 193 coding file

--- day-193.js
+++ day-193.js
@@ -1,8 +1,16 @@
-/* Day 1 - Even or Odd Solution */
-function even_or_odd(number) {
-    if (number%2 == 0) {
-      return "Even";
-    } else {
-      return "Odd";
-    }
-  }
+// Name on billboard
+function billboard(name,price=30){
+  var totalCost = 0;
+  for(i=0; i<name.length; i++){
+      totalCost += price;
+  } 
+  return totalCost;
+};
+
+// Alternative Method 
+function billboard(name,price=30){
+  return name.repeat(price).length;
+} 
+
+// Alternative Method 2
+billboard = (name,price=30)=>[...new Array(name.length)].fill(price).reduce((prev,curr)=>prev+curr)
