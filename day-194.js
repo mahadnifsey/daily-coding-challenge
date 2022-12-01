c4e2cc4f377e3c6f9767748761007f507d0523e1
mahadnifsey <mahadnifsey@gmail.com>
2022-12-01 16:22:35 +0000

    Update to day 194 coding file

--- day-194.js
+++ day-194.js
@@ -1,3 +1,12 @@
-function opposite(n) {
-    return -n;
-  }
+// No Loops 2 - You only need one
+// You will be given an array a and a value x. 
+// All you need to do is check whether the provided array contains the value, without using a loop.
+function check(a,b){
+  return a.includes(b);
+};
+
+// Alternative Method
+const check = (a,x) => a.includes(x);
+
+// Alternative Method 2
+check=(a,x)=>a.includes(x)
