3527fc8ce10395f525dccce6d6e366fd154b7d94
mahadnifsey <mahadnifsey@gmail.com>
2022-12-04 19:59:10 +0000

    Update to day 196 coding file

--- day-196.js
+++ day-196.js
@@ -1,4 +1,23 @@
-/* Convert boolean values to strings 'Yes' or 'No'. */
-function boolToWord( bool ){
-    return bool ? 'Yes':'No';
+// What is between?
+function between(a, b) {
+  // your code here
+  arr = []
+  for(i = a;i <= b; i++){ 
+      arr.push(i)
   }
+  return arr
+};
+
+// Alternative Method
+function between(a, b) {
+  var all = [];
+  for(a;a<=b;a++){
+    all.push(a);
+  }
+  return all;
+};
+
+// Alternative Method 2
+function between(a,b){
+  return Array.from(new Array(b-a +1), x => a++)
+};
