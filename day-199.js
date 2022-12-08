1a5419a3aa9106ba8ac9dd799a307c46f8df8f93
mahadnifsey <mahadnifsey@gmail.com>
2022-12-08 15:13:28 +0000

    Update to day 199 coding file

--- day-199.js
+++ day-199.js
@@ -1,4 +1,8 @@
-// Return Negative
-function makeNegative(num) {
-    return -Math.abs(num);
-  }
+// Type of Sum
+// Return the type of the sum of the two arguments
+function typeOfSum(a,b) {
+  return typeof(a + b);
+};
+
+// Alternative Method
+const typeOfSum = (a,b) => typeof(a+b);
