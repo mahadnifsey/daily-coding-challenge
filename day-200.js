0d520e23fe0133f1e54f8ae478229243cbbf8eba
mahadnifsey <mahadnifsey@gmail.com>
2022-12-09 02:37:58 +0000

    Update to day 200 coding file

--- day-200.js
+++ day-200.js
@@ -1,8 +1,27 @@
-// String Repeat
-// Write a function called repeatStr which repeats the given string string exactly n times.
-function repeatStr (n, s) {
-    return s.repeat(n);
+// Tip Calculator
+// Complete the function, which calculates how much you need to tip based on the total amount of the bill and the service. 
+function calculateTip(amount, rating) {
+  switch(rating.toLowerCase()){
+    case "terrible":return 0;
+    case "poor":return Math.ceil(amount * 0.05);
+    case "good":return Math.ceil(amount * 0.1);
+    case "great":return Math.ceil(amount * 0.15);
+    case "excellent":return Math.ceil(amount * 0.2);
+    default:return "Rating not recognised";
   }
+};
 
-  // Alternative Solution
-  repeatStr = (n, s) => s.repeat(n)
+// Alternative Method
+const TIPS = {
+  "terrible": 0.0,
+  "poor": 0.05,
+  "good": 0.1,
+  "great": 0.15,
+  "excellent": 0.2
+};
+
+const calculateTip = (amount, rating) =>{
+  rating = rating.toLowerCase();
+
+  return rating in TIPS ? Math.ceil(TIPS[rating]*amount): "Rating not recognised";
+};
