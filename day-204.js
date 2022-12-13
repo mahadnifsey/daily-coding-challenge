eed7444daec81fd8bff1bd84529df10d30727469
mahadnifsey <mahadnifsey@gmail.com>
2022-12-13 21:15:27 +0000

    Update to day 204 coding file

--- day-204.js
+++ day-204.js
@@ -1,9 +1,37 @@
-// Grasshopper - Summation.
-// Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
-var summation = function (num) {
-    let sum = 0
-    for(let i = 0; i <= num; i++) {
-        sum += i
+// Leonardo Dicaprio and Oscars
+// You have to write a function that describe Leo.
+function leo(oscar) {
+    if(oscar === 86){
+      return "Not even for Wolf of wallstreet?!";
+    };
+    if(oscar < 88){
+      return "When will you give Leo an Oscar?";
+    };
+    if(oscar === 88){
+      return "Leo finally won the oscar! Leo is happy";
+    };
+    return "Leo got one already!";
+};
+
+// Alternative Method 
+function leo(oscar){
+    switch (oscar){
+      case 88:
+        return "Leo finally won the oscar! Leo is happy";
+        break;
+      case 86:
+        return  "Not even for Wolf of wallstreet?!";
+        break;
+      default:
+        if(oscar<88) return "When will you give Leo an Oscar?";
+        else return  "Leo got one already!";
     }
-    return sum
-}
+};
+
+// Alternative Method 2
+const leo = (oscar) => {
+    return oscar === 88 ? 'Leo finally won the oscar! Leo is happy' :
+           oscar === 86 ? 'Not even for Wolf of wallstreet?!'       :
+           oscar  <  88 ? 'When will you give Leo an Oscar?'        :
+           'Leo got one already!';
+};
