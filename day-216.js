8d310cf18c6f475ad946e5b69285c6671dccf3c8
mahadnifsey <mahadnifsey@gmail.com>
2022-12-29 01:25:48 +0000

    Update to day 216 coding file

--- day-216.js
+++ day-216.js
@@ -1,10 +1,18 @@
-// Parse float
-function parseF(s) {
-    const f = parseFloat(s)
-    return isNaN(f) ? null : f
+// Pythagorean Triple
+function isPythagoreanTriple(integers) {
+    const [a,b,c] = integers.sort((a,b) => a - b)
+    return a**2 + b**2 === c**2
 };
 
 // Alternative Method
-function parseF(s) {
-    return isNaN(parseFloat(s)) ? null : parseFloat(s);
+function isPythagoreanTriple(integers) {
+    let massiv = integers.sort((a, b) => a - b)
+    return (massiv[0] ** 2 + massiv[1] ** 2 == massiv[2] ** 2)
+};
+
+// Alternative Method 2
+function isPythagoreanTriple(i) {
+    var [a, b, c] = i.sort((a, b) => a - b)
+    
+    return (a*a + b*b) === c*c
 };
