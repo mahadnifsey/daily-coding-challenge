dd03fdb88a4a07a7a4bf00046196558d9592098d
mahadnifsey <mahadnifsey@gmail.com>
2023-01-17 22:01:44 +0000

    Update to day 227 coding file

--- day-227.js
+++ day-227.js
@@ -1,1 +1,22 @@
-// Add Code here
+// Localize The Barycenter of a Triangle
+function barTriang(p1, p2, p3){
+    let x = (p1[0] + p2[0] + p3[0]) / 3;
+    let y = (p1[1] + p2[1] + p3[1]) / 3;
+    
+    return [+x.toFixed(4), +y.toFixed(4)];
+};
+
+// Alt Method
+function barTriang(p1, p2, p3){
+    return [+((p1[0]+p2[0]+p3[0])/3).toFixed(4),
+            +((p1[1]+p2[1]+p3[1])/3).toFixed(4)];
+};
+
+// Alt Method 2
+function barTriang(p1, p2, p3){
+  
+    var x = parseFloat(parseFloat((p1[0] + p2[0] + p3[0]) / 3).toFixed(4));
+    var y = parseFloat(parseFloat((p1[1] + p2[1] + p3[1]) / 3).toFixed(4));
+    
+    return [x, y];
+};
