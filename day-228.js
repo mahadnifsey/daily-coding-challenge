85da318d7890378d5c6602e601afced1b1629f8a
mahadnifsey <mahadnifsey@gmail.com>
2023-01-18 02:37:37 +0000

    Update to day 228 coding file

--- day-228.js
+++ day-228.js
@@ -1,1 +1,31 @@
-// Add Code here
+// Barking Mad
+// Use method prototypes to enable all Dogs to bark. 
+function Dog (breed) {
+    this.breed = breed;
+  }
+  
+  var snoopy = new Dog("Beagle");
+  
+  Dog.prototype.bark = function() {
+    return "Woof";
+  };
+  
+  var scoobydoo = new Dog("Great Dane");
+  
+
+// Alt Method
+function Dog (breed) {
+    this.breed = breed;
+  }
+  
+  var snoopy = new Dog("Beagle");
+  
+  snoopy.bark = function() {
+    return "Woof";
+  };
+  
+  var scoobydoo = new Dog("Great Dane");
+  
+  scoobydoo.bark = function() {
+    return "Woof";
+};
