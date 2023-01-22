481dda387aad664be8a5b5d8cc56de5cb4af9c45
mahadnifsey <mahadnifsey@gmail.com>
2023-01-22 04:07:44 +0000

    Add day 232 coding file

---
+++ day-232.js
@@ -0,0 +1,10 @@
+// Template Strings
+var TempleStrings = function(obj, feature) {
+    return `${obj} are ${feature}`;
+};
+
+// Alt Method
+let TempleStrings = (obj, feature) => `${obj} are ${feature}`;
+
+// Alt Method 2
+const TempleStrings = (obj, feature) => `${obj} are ${feature}`;
