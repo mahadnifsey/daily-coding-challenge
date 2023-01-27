0ac6ffdf46a197a4cff84e977e70aff2b0f6a348
mahadnifsey <mahadnifsey@gmail.com>
2023-01-27 01:26:00 +0000

    Update to day 234 coding file

--- day-234.js
+++ day-234.js
@@ -1,1 +1,19 @@
-// Add code here
+// Enumerable Magic #1 - True for All?
+function all(arr, fun) {
+    return arr.every(fun);
+};
+
+// Alternative Method
+function all(arr, fun)
+{
+  var length = arr.length;
+  
+  for (var i=0; i<length; ++i)
+    if (!fun(arr[i]))
+      return false;
+  
+  return true;
+};
+
+// Alternative Method 2
+const all = ( arr, fun ) => arr.every( fun );
