6e4a92a878c8e17d4e7cb25f2aabd55c65838b4d
mahadnifsey <mahadnifsey@gmail.com>
2023-01-28 11:00:37 +0000

    Update to day 235 coding file

--- day-235.js
+++ day-235.js
@@ -1,1 +1,13 @@
-// Add code here
+// Quadratic Coefficients Solver
+function quadratic(x1, x2){
+    return [1, -(x2 + x1), (x1 * x2)]
+};
+
+// Alternative Method
+const quadratic = (x1, x2) => [
+    1,
+    -x1 -x2,
+    x1 * x2
+]
+// Alternative Method 2
+let quadratic = (x1, x2) => [1, -(x1+x2), x1*x2];
