1880e2d8944ac06faf2869b2d972cbc4e918639c
mahadnifsey <mahadnifsey@gmail.com>
2023-01-29 03:23:03 +0000

    Update to day 236 coding file

--- day-236.js
+++ day-236.js
@@ -1,1 +1,20 @@
-// Add code here
+// ASCII Total
+function uniTotal(str) {
+    var count = 0;
+    for (var i = 0; i < str.length; i++) {
+      count += str.charCodeAt(i);
+    }
+    return count;
+};
+
+// Alt Method  
+function uniTotal (string) {
+    return string.split('').reduce((prev,curr) => prev + curr.charCodeAt(), 0)
+}
+
+// Alt Method 2
+function uniTotal(str){
+    return str.split('').reduce(function(last,now){
+      return last+now.charCodeAt();
+    },0);
+};
