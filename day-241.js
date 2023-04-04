cf4d66e07870c5b2d7741dff736326b72842ab8e
mahadnifsey <mahadnifsey@gmail.com>
2023-04-04 08:38:28 +0100

    Add day 241 coding file

---
+++ day-241.js
@@ -0,0 +1,11 @@
+// get character from ASCII Value
+// Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+function getChar(c) {
+    return String.fromCharCode(c);
+};
+
+// Alt Method
+const getChar = c => String.fromCharCode(c);
+
+// Alt Method 2
+getChar = c => String.fromCharCode(c);``
